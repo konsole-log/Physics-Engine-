69f736d0955d5f08e74d3491faaa4907422022db
konsole-log <anushshrestha00@gmail.com>
2026-06-04 11:44:17 +0545

    Updated main

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
 #include "raylib.h"
-
+#include <iostream>
+using namespace std;
 int main() {
-    InitWindow(800, 450, "");
-    SetTargetFPS(60);
+  InitWindow(800, 450, "");
+  SetTargetFPS(60);
+  Vector2 position = {1.0f, 1.0f};
+  cout << position.x << position.y;
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(BLACK);
+    EndDrawing();
+  }
 
-    while (!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground(BLACK);
-        EndDrawing();
-    }
-
-    CloseWindow();
-    return 0;
+  CloseWindow();
+  return 0;
 }
