055b09b90cc2b9a79dd61b3fda541c8c06872923
konsole-log <anushshrestha00@gmail.com>
2026-06-04 11:49:50 +0545

    updated main

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,12 @@ using namespace std;
 int main() {
   InitWindow(800, 450, "");
   SetTargetFPS(60);
-  Vector2 position = {1.0f, 1.0f};
+  Vector2 position = {20.0f, 10.0f};
   cout << position.x << position.y;
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(BLACK);
+    DrawCircle(position.x, position.y, 20.0f, RED);
     EndDrawing();
   }
 
