340895e6b26682d162993d71621aa6decc2cab1b
konsole-log <anushshrestha00@gmail.com>
2026-09-07 10:26:47 +0545

    for test

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,36 @@
 #include "raylib.h"
-#include <iostream>
+#include <cmath>
 using namespace std;
+const int screenWidth = 800;
+const int screenHeight = 450;
+class Complex {
+private:
+  float x, y, r, ang;
+
+public:
+  Complex(float x, float y) {
+    this->x = x;
+    this->y = y;
+  }
+  ~Complex() {}
+  float calculateRadius() { return sqrt(x * x + y * y); }
+  float calculateAngle() { return atan(y / x); }
+  void drawComplexPoint() {
+    DrawCircleV({x + screenWidth, y - screenHeight}, 5.0f, RED);
+    DrawLineV({(float)screenWidth / 2, (float)screenHeight / 2},
+              {x + screenWidth, y - screenHeight}, WHITE);
+  }
+};
+
 int main() {
-  InitWindow(800, 450, "");
+  Complex c(10.0f, 20.0f);
+  InitWindow(800, 450, "Complex Number Graphing");
   SetTargetFPS(60);
-  Vector2 position = {20.0f, 10.0f};
-  cout << position.x << position.y;
   while (!WindowShouldClose()) {
     BeginDrawing();
-    ClearBackground(BLACK);
-    DrawCircle(position.x, position.y, 20.0f, RED);
+    DrawGrid(10, 20.0f);
+    c.drawComplexPoint();
     EndDrawing();
   }
-
   CloseWindow();
-  return 0;
 }
